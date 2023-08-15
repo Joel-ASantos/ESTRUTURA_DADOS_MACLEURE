239899d82e112c2749d6675a5df0121d4bd8e883
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-08-15 18:55:17 -0300

    atividade de verificar numero primo concluida

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 
-int verificarNumeroPrimo(int n){
-    if(n < 2){
-        printf("Ele nao eh primo");
-    }else{
-        //Ciclo for não finalizado
-        for(;;){
+int verificarNumeroPrimo(int n)
+{   
+    if(n % 2 == 0){
+        return 0;
+    }
 
+    for(int i = 2; i <= sqrt(n);i++){
+        if(n % i == 0){
+            return 0;
         }
     }
+
+    return 1;
 }
 
-int main(int argc,char*argv[]){
+int main(int argc,char*argv[])
+{
     int numero;
 
-    printf("Digite um numero: ");
+    printf("Digite o numero: ");
     scanf("%d",&numero);
 
-    printf(verificarNumeroPrimo(numero));
+    if(verificarNumeroPrimo(numero)>=1){
+        printf("eh primo");
+    }else if(verificarNumeroPrimo(numero)<1){
+        printf("nao eh primo");
+    }
     return 0;
 }
