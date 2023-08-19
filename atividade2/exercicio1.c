9b291709b49793885f8293662f737aaa9bdee146
Joel-ASantos <joel.alves.santos77@gmail.com>
2023-08-19 11:04:17 -0300

    Continuar em casa

---
+++ atividade2/exercicio1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Soma dos elementos de um array. 
+Escreva uma função que recebe um array de números inteiros 
+e retorna a soma de todos os elementos;
+*/
+
+int main(int args, char*argv[]){
+    int numeros[5];
+
+    printf("Digite 5 numeros: ");
+    scanf("%d",&numeros[5]);
+
+    Somando_Array(numeros[5]):
+    return 0;
+}
+int Somando_Array(int array[5]){
+    for(int i = 0; i < 5; i++){
+        
+    }
+}
