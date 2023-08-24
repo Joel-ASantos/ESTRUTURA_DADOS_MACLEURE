7509d54fa4d2dde3c185570ae751f06ab3a98123
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-08-24 15:43:10 -0300

    adicionando o .gitignore

---
+++ atividade2/exercicio3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ Verificar se um elemento existe no array 
+ Escreva uma função que recebe um array de strings e uma string de busca,
+ e retorna "1" se a string de busca existe no array, ou "0" caso não exista.
+*/
+
+int main(int args, char* argv[]){
+    return 0;
+}
