4409a588b45e5d008b61959a2086cec33ae6b648
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-09-19 17:17:05 -0300

    iniciando_Algoritmo

---
+++ Lista2/atividade5/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+No seu repositório de atividades, crie uma pasta chamada atividade5 que contenha os arquivos main.c bubble_sort.h e bubble_sort.c;
+
+O seu programa deve implementar o algoritmo bubble sort para ordenar um arquivo de palavras passados
+na chamada do programa e criar um arquivo com as palavras ordenadas como saída.
+Exemplo:
+./main arq_palavras.txt
+Saída: arq_palavras_ordenado.txt
+
+arq_palavras.txt
+hoje
+amanha
+depois
+
+arq_palavras_ordenado.txt
+amanha
+depois
+hoje
+
+OBS: o nome do arquivo é passado como argumento na chamada do programa (argv);*/
+
+int main(int argc, char *argv[]){
+    printf("Hello, World!");
+    return 0;
+}
