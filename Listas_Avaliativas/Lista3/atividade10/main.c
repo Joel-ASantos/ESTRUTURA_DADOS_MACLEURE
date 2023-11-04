2eb80c8ebafab0376a29e7449b9d86a5e17ffaa1
Joel-Alves-Santos <joel.alves.santos77@gmail.com>
2023-11-03 23:47:32 -0300

    add

--- Listas_Avaliativas/Lista3/atividade10/main.c
+++ Listas_Avaliativas/Lista3/atividade10/main.c
@@ -4,22 +4,25 @@
 #include "tabela_hash.h"
 
 int main(int argc,char *argv[]){
-    //Imput
+    
     hash_table_put("Teste1","Valor1");
     hash_table_put("Teste2","Valor2");
     hash_table_contains("Teste1");
     
-    /*---------------------GET--------------------*/
     printf("Get: %s",hash_table_get("Teste1"));
     
-    /*---------------------CONTAIN--------------------*/
     printf("\nContains: %s",hash_table_contains("Valor1"));
     printf("\nContains: %s",hash_table_contains("Valor2"));
 
-    /*---------------------REMOVE--------------------*/
     hash_table_remove("Teste2");
-
-    /*---------------------CONTAIN--------------------*/
+    
     printf("\nContains: %s",hash_table_contains("Valor1"));
+
+    /*----------------------ArrayIndex---------------------------*/
+    int in = hash_code("Teste1");
+    hash_table_put("Teste1","Valor1");
+
+    printf("Hash: %s",in);
+
     exit(0);
 }
